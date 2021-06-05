7e9035640b80878a56f14a1a9b88f10b4c4fb29c
Law <Gennlaw@hotmail.com>
2021-06-05 12:46:18 +0300

    lesson-21 Dialogs markup results

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,29 @@ import s from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+                <div className={s.dialogsItems}>
+                    <div className={`${s.dialog} ${s.active}`}>
+                       User 1 
+                    </div>
+                    <div className={s.dialog}>
+                       User 2 
+                    </div>
+                    <div className={s.dialog}>
+                       User 3 
+                    </div>
+                    <div className={s.dialog}>
+                       User 4 
+                    </div>
+                    <div className={s.dialog}>
+                       User 5 
+                    </div>
+                </div>
+                <div className={s.messages}>
+                    <div className={s.message}>Hello</div>
+                    <div className={s.message}>Test</div>
+                    <div className={s.message}>These Messages</div>
+                </div>
         </div>
     )
 }
