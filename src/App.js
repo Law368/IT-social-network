371c6d2ac299eab4ee57a84d815d3c0b086d0884
Law <Gennlaw@hotmail.com>
2021-06-05 11:57:53 +0300

    lesson-19 Route results

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
 import React from 'react';
+import {BrowserRouter, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header.jsx';
 import Navbar from './components/Navbar/Navbar.jsx';
 import Profile from './components/Profile/Profile.jsx';
 import Dialogs from './components/Dialogs/Dialogs.jsx';
+import News from './components/News/News.jsx';
+import Music from './components/Music/Music.jsx';
+import Settings from './components/Settings/Settings.jsx';
 
 function App() {
   return (
-    <div className='app-wrapper'>
-      <Header /> 
-      <Navbar />
-      {/* <Profile /> */}
-      <div className='app-wrapper-content'>
-      <Dialogs />
+    <BrowserRouter>
+      <div className='app-wrapper'>
+        <Header /> 
+        <Navbar />
+        <div className='app-wrapper-content'>
+          <Route path='/dialogs' component={Dialogs}/>
+          <Route path='/profile' component={Profile}/>
+          <Route path='/news' component={News}/>
+          <Route path='/music' component={Music}/>
+          <Route path='/settings' component={Settings}/>
+        </div>
       </div>
-    </div>
+    </BrowserRouter>
   );
 }
 
