8cf7071b1d8d0a985c8f58283f570aea60f8e461
Law <Gennlaw@hotmail.com>
2021-06-09 20:37:27 +0300

    lesson-29 STATE  results

---
+++ src/redux/state.js
@@ -0,0 +1,32 @@
+let state = {
+      profilePage: {
+        posts: [
+            {id: 1, message: 'Hi, How are you', likesCount: 12},
+            {id: 2, message: 'First post', likesCount: 11},
+            {id: 3, message: '111', likesCount: 20},
+            {id: 4, message: '2222', likesCount: 35},
+            {id: 5, message: '3333', likesCount: 33},
+            {id: 6, message: '44444', likesCount: 27}
+          ]
+      },
+      dialogsPage: {
+        messages: [
+            {id: 1, message: 'Hi'},
+            {id: 2, message: 'Test'},
+            {id: 3, message: '111'},
+            {id: 4, message: '2222'},
+            {id: 5, message: '3333'},
+            {id: 6, message: '44444'}
+          ],
+        dialogs: [
+            {id: 1, name: 'User 1'},
+            {id: 2, name: 'User 2'},
+            {id: 3, name: 'User 3'},
+            {id: 4, name: 'User 4'},
+            {id: 5, name: 'User 5'},
+            {id: 6, name: 'User 6'}
+          ]
+      }
+  }
+
+  export default state;
