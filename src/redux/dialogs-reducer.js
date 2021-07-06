cb58893aedff875b6d58ef9f7d7deb764d45bb81
Nikita <zhulev@e-krit.ru>
2021-07-06 13:11:08 +0300

    lesson-42 results

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,27 @@
 const UPDATE_NEW_MESSAGE_BODY = "UPDATE_NEW_MESSAGE_BODY";
 const SEND_MESSAGE = 'SEND_MESSAGE';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+    messages: [
+        { id: 1, message: 'Hi' },
+        { id: 2, message: 'Test' },
+        { id: 3, message: '111' },
+        { id: 4, message: '2222' },
+        { id: 5, message: '3333' },
+        { id: 6, message: '44444' }
+    ],
+    dialogs: [
+        { id: 1, name: 'User 1' },
+        { id: 2, name: 'User 2' },
+        { id: 3, name: 'User 3' },
+        { id: 4, name: 'User 4' },
+        { id: 5, name: 'User 5' },
+        { id: 6, name: 'User 6' }
+    ],
+    newMessageBody: '',
+}
+
+const dialogsReducer = (state = initialState, action) => {
     switch (action.type) {
         case UPDATE_NEW_MESSAGE_BODY:
             state.newMessageBody = action.body;
--- src/redux/profile-reducer.js
+++ src/redux/profile-reducer.js
@@ -1,7 +1,18 @@
 const ADD_POST = 'ADD-POST';
 const UPDATE_NEW_POST_TEXT = "UPDATE-NEW-POST-TEXT";
 
-const profileReducer = (state, action) => {
+let initialState = {
+    posts: [
+        { id: 1, message: 'Hi, How are you', likesCount: 12 },
+        { id: 2, message: 'First post', likesCount: 11 },
+        { id: 3, message: '111', likesCount: 20 },
+        { id: 4, message: '2222', likesCount: 35 },
+        { id: 5, message: '3333', likesCount: 33 },
+        { id: 6, message: '44444', likesCount: 27 }],
+    newPostText: 'TESTtestTEST'
+}
+
+const profileReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST:
             let newPost = {
