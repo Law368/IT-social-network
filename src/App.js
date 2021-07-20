1d5bd9875c3c1f4cd911d4d763b1d45aac3ecdce
Nikita <zhulev@e-krit.ru>
2021-07-20 19:48:02 +0300

    lesson-38-DIY results

--- src/App.js
+++ src/App.js
@@ -21,8 +21,7 @@ function App(props) {
           <Route path='/dialogs' render={() => <Dialogs state={props.state.dialogsPage} />} />
           <Route path='/profile' render={() => <Profile
               profilePage={props.state.profilePage}
-              addPost={props.addPost}
-              updateNewPostText={props.updateNewPostText}/>} />
+              dispatch={props.dispatch}/>} />
           <Route path='/news' render={() => <News />} />
           <Route path='/music' render={() => <Music />} />
           <Route path='/settings' render={() => <Settings />} />
